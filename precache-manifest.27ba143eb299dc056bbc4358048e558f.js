87f80c8acc010539b286b1efd73eb81287bcc73b
Timur Zakirov <1234tima.g@gmail.com>
2020-05-19 15:32:34 +0300

    Updates

---
+++ precache-manifest.27ba143eb299dc056bbc4358048e558f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4f957a7b8e7b438543f2db35076f45",
+    "url": "/domashka05/index.html"
+  },
+  {
+    "revision": "9201c9f321c482ef7141",
+    "url": "/domashka05/static/css/main.984df818.chunk.css"
+  },
+  {
+    "revision": "4808be5ba907ff8dd6e1",
+    "url": "/domashka05/static/js/2.7e91ee22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/domashka05/static/js/2.7e91ee22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9201c9f321c482ef7141",
+    "url": "/domashka05/static/js/main.8f700867.chunk.js"
+  },
+  {
+    "revision": "8ba6f9dbacfbf29829d1",
+    "url": "/domashka05/static/js/runtime-main.32150345.js"
+  }
+]);
